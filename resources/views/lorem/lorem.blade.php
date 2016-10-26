35175b4be014dd724f337c809f2f820c5daa7a39
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-10-26 05:38:19 -0400

    fixed my lorem view file which had been corrupted

--- resources/views/lorem/lorem.blade.php
+++ resources/views/lorem/lorem.blade.php
@@ -1,4 +1,4 @@
- @extends('layouts.master')
+   @extends('layouts.master')
 
 @section('head')
 @endsection
@@ -6,57 +6,48 @@
 @section('title', 'Developers Best Friend')
 
 @section('content')
-     
-how many users to generate?
-<form method='POST' id="keep" action='/usergen'>
-    {{ csrf_field() }}
-    <input type = "checkbox" name ="icon"> include icon?
-    <input type = "checkbox" name ="profile"> include profile?
-    <input type='text' name='users'>
+
+how many paragraphs pf lorem ipsum?
+<form method='POST' id="keep" action='/lorem'>
+    {{ csrf_field()}}
+    <input type="radio" name="switch" value="standard"> latin
+
+    <input type="radio" name="switch" value="customEng"> english <br>
+
+    <input type='text' name='paragraphs' value='{{old("paragraphs")}}'>
     <input type='submit' value='Submit'>
 </form>
- 
-    
-       <h1> Fake User Generator </h1>
-      
+
+                <div>
+       <h1> Lorem Ipsum Generator </h1>
+
        @if(count($errors) > 0)
         <ul>
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
         </ul>
-       @endif
-             
-       
-   
-       @if (isset($usergens)) 
-        <article id="left">
+        @endif
+       @if (isset($contents))
+        <article id="left" class="lorem">
+        <h2> Here is your Lorem Ipsum text: </h2>
+          {!! $contents !!}
 
-        <?php $userstring = json_decode($usergens); ?>
-        <h2> Here are your users: </h2>
-        <?php foreach ($userstring as $user) {
-                echo '<p>';
-                echo 'name: '.$user->firstname.' '.$user->lastname.' <br>';
-                echo 'address: '.$user->address.' <br>';
-                echo 'phone: '.$user->phone.'<br>';
-                echo 'email: '.$user->email.'<br>';
-                echo 'passsord: '.htmlentities($user->password).'<br>';
-                echo 'initials: '.$user->initials.'<br>';
-                echo 'profile: '.$user->profiletext;
-                echo 'avatar: '.$avatar;
-                echo '</p>';
-            }
-             
-        ?>
-        
+
         </article>
 
-        <article id="right">
-        <h2> Here are your users in JSON format: </h2>
-        <!--<textarea> {!! $usergens!!} </textarea>-->
-        <textarea> {{$usergens}} </textarea>
+        <article id="right" class="lorem">
+        <h2> Here is your Lorem Ipsul text with Paragraph tags: </h2>
+        @foreach($contentsarray as $paragraph)
+            {{"<p>"}} {{$paragraph}} {{"</p>"}}<p></p>
+
+        @endforeach
+
+
+
         </article>
-        
+        </div>
+
        @endif
 
 @stop
